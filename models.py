2459cf323130a8357d5f6eba12dc1c70fac39e5d
szemenyeim <szemenyeim@gmail.com>
2019-11-16 17:40:22 +0100

    Inputs work

--- models.py
+++ models.py
@@ -9,12 +9,19 @@ class ActionBlock(nn.Module):
         self.scale = None
 
         if means is not None:
-            self.means = torch.Tensor(means).cuda()
-            self.scale = torch.Tensor(scale).cuda()
+            self.means = torch.Tensor(means)
+            self.scale = torch.Tensor(scale)
             assert (len(means) == len(scale) and len(means) == actions)
 
         self.Layer = nn.Linear(features,actions)
         self.activation = nn.Softmax(dim=1) if type == 'cat' else nn.Sigmoid()
+
+    def _apply(self, fn):
+        super(ActionBlock, self)._apply(fn)
+        if self.means is not None:
+            self.means = fn(self.means)
+            self.scale = fn(self.scale)
+        return self
 
     def forward(self, x):
         x = self.activation(self.Layer(x))
@@ -31,3 +38,54 @@ class ActionLayer(nn.Module):
     def forward(self, x):
         outs = [block(x) for block in self.blocks]
         return outs
+
+class EmbedBlock(nn.Module):
+    def __init__(self,inputs,features):
+        super(EmbedBlock,self).__init__()
+
+        self.Layer = nn.Linear(inputs,features)
+        self.relu = nn.LeakyReLU(0.1)
+
+    def forward(self,x):
+        if x.dim() == 1:
+            if x.shape[0] == 0:
+                return None
+            x = x.unsqueeze(0)
+        return self.relu(self.Layer(x))
+
+class InputLayer(nn.Module):
+    def __init__(self,inputs,features):
+        super(InputLayer,self).__init__()
+
+        if len(inputs) != 4:
+            raise Exception("Image and Full observation types are not yet supported")
+
+        inputNums = inputs[-1]
+
+        self.blocks = nn.ModuleList([EmbedBlock(input,features) for input in inputNums])
+
+    def forward(self, x):
+        device = next(self.parameters()).device
+
+        outs = [[[block(torch.Tensor(obj).to(device)) for block,obj in zip(self.blocks,player)] for player in time] for time in x]
+
+        outs = [[torch.cat([t for t in player if t is not None],dim=0) for player in time] for time in outs]
+
+        # Change list from [time x nPlayer x objNum x features] to [nPlayers x time x objNum x features]
+        outs = list(map(list, zip(*outs)))
+
+        return outs
+
+class TestNet(nn.Module):
+    def __init__(self,inputs,action,feature):
+        super(TestNet,self).__init__()
+
+        self.InNet = InputLayer(inputs,feature)
+        self.OutNet = ActionLayer(feature,action)
+
+    def forward(self, x):
+        features = self.InNet(x)
+        features = [sum([torch.mean(f,dim=0) for f in feature])/len(feature) for feature in features]
+        features = torch.stack(features)
+        actions = self.OutNet(features)
+        return actions
--- testNet.py
+++ testNet.py
@@ -4,18 +4,18 @@ import torch
 
 def doRoboCup():
     nPlayers = 5
-    env = DynEnv.RoboCupEnvironment(nPlayers=nPlayers, render=False, observationType=DynEnv.ObservationType.Partial,
+    env = DynEnv.RoboCupEnvironment(nPlayers=nPlayers, render=True, observationType=DynEnv.ObservationType.Partial,
                                     noiseType=DynEnv.NoiseType.Realistic, noiseMagnitude=2)
     observations = env.setRandomSeed(42)
 
     batch, action = env.getActionSize()
+    inputs = env.getObservationSize()
     feature = 128
-    Net = ActionLayer(feature,action).cuda()
+    Net = TestNet(inputs,action,feature).cuda()
 
     while True:
 
-        inputs = torch.randn(batch,feature).cuda()
-        actions = Net(inputs)
+        actions = Net(observations)
 
         actions = torch.stack([torch.argmax(actions[0],dim=1).float(),
                             torch.argmax(actions[1],dim=1).float(),
@@ -23,7 +23,6 @@ def doRoboCup():
                             actions[3].squeeze()],dim=1)
 
 
-        #action = Net(observations)
         state,observations,trewards,rrewads,finished = env.step(actions.detach().cpu())
         if finished:
             observations = env.reset()
@@ -33,17 +32,16 @@ def doDrive():
     env = DynEnv.DrivingEnvironment(nPlayers=nPlayers, render=True, observationType=DynEnv.ObservationType.Partial,
                                     noiseType=DynEnv.NoiseType.Realistic, noiseMagnitude=2)
 
+    observations = env.setRandomSeed(42)
 
     batch, action = env.getActionSize()
+    inputs = env.getObservationSize()
     feature = 128
-    Net = ActionLayer(feature,action).cuda()
+    Net = TestNet(inputs,action,feature).cuda()
 
     while True:
 
-        inputs = torch.randn(batch,feature).cuda()
-        actions = Net(inputs)[0]
-
-        #action = Net(observations)
+        actions = Net(observations)[0]
         state, observations, trewards, rrewads, finished = env.step(actions.detach().cpu())
         if finished:
             observations = env.reset()
