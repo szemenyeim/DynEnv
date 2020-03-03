9edc00f6b3a807f329b3249734fbdcbd6365b400
Márton Szemenyei <szemenyeim@gmail.com>
2020-03-03 06:48:15 +0100

    Circular dependency removed

--- DynEnv/__init__.py
+++ DynEnv/__init__.py
@@ -1,6 +1,29 @@
-from .cutils import ObservationType, NoiseType, DynEnvType, make_dyn_env
+from .cutils import ObservationType, NoiseType, DynEnvType
 from .RoboCupEnvironment import RoboCupEnvironment
 from .DrivingEnvironment import DrivingEnvironment
+from stable_baselines.common.vec_env import SubprocVecEnv
 
+def make_dyn_env(env, num_envs, num_players, render, observationType, noiseType, noiseMagnitude,
+                 use_continuous_actions):
+    if env is DynEnvType.ROBO_CUP:
+        envs = [lambda: RoboCupEnvironment(nPlayers=num_players, render=render, observationType=observationType,
+                                           noiseType=noiseType, noiseMagnitude=noiseMagnitude,
+                                           allowHeadTurn=use_continuous_actions,
+                                           obs_space_cast=True)
+                for _ in range(num_envs)]
+        env = SubprocVecEnv(envs)
+        name = "RoboCup"
+    elif env is DynEnvType.DRIVE:
+        envs = [lambda: DrivingEnvironment(nPlayers=num_players, render=render, observationType=observationType,
+                                           noiseType=noiseType, noiseMagnitude=noiseMagnitude,
+                                           continuousActions=use_continuous_actions,
+                                           obs_space_cast=True)
+                for _ in range(num_envs)]
+        env = SubprocVecEnv(envs)
+        name = "Driving"
+    else:
+        raise ValueError
+
+    return env, name
 
 __all__ = ["DrivingEnvironment","RoboCupEnvironment","ObservationType","NoiseType","make_dyn_env","DynEnvType"]
--- DynEnv/cutils.py
+++ DynEnv/cutils.py
@@ -5,34 +5,6 @@ from enum import IntEnum
 
 import numpy as np
 from pymunk import Vec2d, Body
-from stable_baselines.common.vec_env import SubprocVecEnv
-
-from .DrivingEnvironment import DrivingEnvironment
-from .RoboCupEnvironment import RoboCupEnvironment
-
-
-def make_dyn_env(env, num_envs, num_players, render, observationType, noiseType, noiseMagnitude,
-                 use_continuous_actions):
-    if env is DynEnvType.ROBO_CUP:
-        envs = [lambda: RoboCupEnvironment(nPlayers=num_players, render=render, observationType=observationType,
-                                           noiseType=noiseType, noiseMagnitude=noiseMagnitude,
-                                           allowHeadTurn=use_continuous_actions,
-                                           obs_space_cast=True)
-                for _ in range(num_envs)]
-        env = SubprocVecEnv(envs)
-        name = "RoboCup"
-    elif env is DynEnvType.DRIVE:
-        envs = [lambda: DrivingEnvironment(nPlayers=num_players, render=render, observationType=observationType,
-                                           noiseType=noiseType, noiseMagnitude=noiseMagnitude,
-                                           continuousActions=use_continuous_actions,
-                                           obs_space_cast=True)
-                for _ in range(num_envs)]
-        env = SubprocVecEnv(envs)
-        name = "Driving"
-    else:
-        raise ValueError
-
-    return env, name
 
 
 class DynEnvType(IntEnum):
