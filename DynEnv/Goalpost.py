edcf2ee755fb70b7d4694038cd3875ee42d14628
Márton Szemenyei <szemenyeim@gmail.com>
2020-04-13 18:31:04 +0200

    Goalpost dir and side added

--- DynEnv/Goalpost.py
+++ DynEnv/Goalpost.py
@@ -2,10 +2,13 @@ from .cutils import CollisionType
 from pymunk import Body, Circle
 
 class Goalpost(object):
-    def __init__(self,x,y, radius):
+    radius = 5
+    def __init__(self,x,y, side, dir):
         body = Body(0, 0, Body.STATIC)
         body.position = x, y
-        self.shape = Circle(body, radius*2, (0, 0))
+        self.side = side
+        self.dir = dir
+        self.shape = Circle(body, self.radius*2, (0, 0))
         self.shape.color = (0, 0, 255)
         self.shape.elasticity = 0.95
         self.shape.collision_type = CollisionType.Goalpost
