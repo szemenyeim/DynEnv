30d8bd423db3dbbbfb324da8951c45ef5bc86420
Márton Szemenyei <szemenyeim@gmail.com>
2020-04-29 16:43:34 +0200

    action transform for RoboCup

--- DynEnv/utils/utils.py
+++ DynEnv/utils/utils.py
@@ -15,6 +15,23 @@ from ..environment_base import PredictionDescriptor
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
+def transformActions(actions, discreteTurn = False):
+    newActions = actions.clone()
+    newActions[:, 2] = newActions[:, 0].clone()
+    turns = newActions[:, 1]
+    turns[turns == 2] = -1
+    forwards = newActions[:, 0]
+    forwards[forwards < 3] = 0
+    forwards[forwards == 3] = 1
+    forwards[forwards == 4] = -1
+    sides = newActions[:, 2]
+    sides[sides > 2] = 0
+    sides[sides == 2] = 1
+    sides[sides == 1] = -1
+    if discreteTurn:
+        newActions[:,3] -= 3
+    return newActions
+
 class AttentionType(Enum):
     SINGLE_ATTENTION = 0
     DOUBLE_ATTENTION = 1
