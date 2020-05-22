60b48649e0ece53e7e3d9e3a240b2264cb126a7b
Márton Szemenyei <szemenyeim@gmail.com>
2020-05-22 09:14:48 +0200

    Train script fix argument error fixed Now launches tmux launches 4 parallel runs only

--- DynEnv/examples/train.sh
+++ DynEnv/examples/train.sh
@@ -1,6 +1,7 @@
-for seed in 42 # 658 94846 341 35710 4380 5646 83465 97 4162
+tmux new-session -d -s RL
+for seed in 42 658 94846 341 #35710 4380 5646 83465 97 4162
 do
-    NOTE="baseline with seed=${seed}"
+    NOTE="seed-${seed}-"
     ARGS=""
 
     for arg in "$@"
@@ -8,29 +9,35 @@ do
         if [ "$arg" == "--partial" ]
         then
             ARGS+=" --observationType ObservationType.PARTIAL"
-            NOTE+=" using partial observations"
+            NOTE+="PartialObs-"
         elif [ "$arg" == "--full" ]
         then
             ARGS+=" --observationType ObservationType.FULL"
-            NOTE+=" using full observations"
+            NOTE+="FullObs-"
         fi
         
         if [ "$arg" == "--reconstruction" ]
         then
             ARGS+=" --use-reconstruction True --recon-pretrained True"
-            NOTE+=" using reconstruction"
+            NOTE+="Recon-"
         fi
         
         
         if [ "$arg" == "--longterm" ]
         then
             ARGS+=" --long-horizon-coeff 1e-3"
-            NOTE+=" using long-term prediction"
+            NOTE+="LongTermPred"
         else
             ARGS+=" --long-horizon-coeff 0.0"
         fi
     done
-    echo "$NOTE"
-    echo "$ARGS"
-    python ./main.py --seed $seed --note $NOTE $ARGS &
+   # echo "$seed"
+   # echo "$NOTE"
+   # echo "$ARGS"
+   # echo "python3 -W ignore main.py --seed $seed --note $NOTE $ARGS"
+   CMD="python3 -W ignore main.py --seed ${seed} --note ${NOTE} ${ARGS}"
+   echo "$CMD"
+   tmux new-window -t RL:$seed
+   tmux send-keys -t RL:$seed "$CMD" C-m
+   sleep 0.5
 done
