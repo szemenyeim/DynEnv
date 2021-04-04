b43f06d29e83f79c5ceba75ba6aba0c8327079bc
rpatrik96 <prmedia17@gmail.com>
2021-04-04 21:45:44 +0200

    detach fix

--- DynEnv/models/actor_critic.py
+++ DynEnv/models/actor_critic.py
@@ -150,7 +150,10 @@ class A2CNet(nn.Module):
         policies_old = self.actor_old(features)
         _, _, log_probs_old = self._calc_log_probs(policies_old)
 
-        return log_probs_old.detach()
+        # detach
+        log_probs_old = [l.detach() for l in log_probs_old]
+
+        return log_probs_old
 
 
 # Complete action layer for multiple action groups
